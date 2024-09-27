06d4d56062fb589ea041e814d083f3782991ddb7
Payal Sharma <payalsharmareal@gmail.com>
2024-09-27 11:09:14 +0530

    validation applied in user model

--- Backend/model/usermodel.js
+++ Backend/model/usermodel.js
@@ -4,15 +4,33 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
         type: String,
+        trim: true,
         required: true,
     },
     email : {
         type: String,
         required: true,
+        validate: ({
+            validator: function(email){
+                if( typeof email != undefined){
+                    let isValidEmail = validator.isEmail(email)
+                    return isValidEmail
+                }else{
+                    console.error("email is undefined")
+                    return false
+                }
+            }
+        },{}),
+        unique: true
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        validate: ({validator: function(v){
+            return v.length > 8
+        }},{message: function(props){
+            return `${props.path} must have length 8 but got ${props.value}`
+        }})
     }
 },{timestamps: true});
 
